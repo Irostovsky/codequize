d79c615e3c8333472d3ea1430db71323adc9cdd3
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2018-03-17 14:22:07 +0200

    First version of eratosfen.

--- lib/eratosfen.rb
+++ lib/eratosfen.rb
@@ -7,4 +7,17 @@ class Eratosfen
   def initialize(max_number)
     @max_number = max_number
   end
+
+  def call
+    return [] if max_number < 2
+    arr = (2..max_number).to_a
+    i = arr.first
+    while i <= Math.sqrt(max_number) do
+      arr.reject!{|el| (el % i).zero? && el > i }
+      i += 1
+    end
+    arr
+  end
+
+
 end
--- spec/eratosfen_spec.rb
+++ spec/eratosfen_spec.rb
@@ -4,12 +4,35 @@ require 'eratosfen'
 
 describe Eratosfen do
   subject { described_class.new(max_number) }
-  describe '#call' do
+
+  describe '#init' do
     let(:max_number) { 2 }
 
-    it 'init' do
+    it {
       expect(subject.max_number).to eq max_number
+    }
+  end
+
+  describe '#call' do
+    context 'not allowed values les then 2' do
+      let(:max_number) { 1 }
+      it "return min res" do
+        expect(subject.call).to be_empty
+      end
     end
 
+    context 'min value' do
+      let(:max_number) { 2 }
+      it "return min res" do
+        expect(subject.call).to eq [2]
+      end
+    end
+
+    context '17' do
+      let(:max_number) { 17 }
+      it "return simple" do
+        expect(subject.call).to eq [2, 3, 5, 7, 11, 13, 17]
+      end
+    end
   end
 end
